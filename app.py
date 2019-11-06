47791dc1d12c4de8d50b2b957cbddd8dfaf7b4c0
mariotcosio <mariotcosio@gmail.com>
2019-11-05 22:06:37 -0600

    final commit, for realz this time

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+import Misssion_to_Mars
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -13,9 +13,10 @@ def index():
 
 @app.route("/scrape")
 def scraper():
-    mars = mongo.db.mars_data= scrape_mars.scrape_all()
+    mars = mongo.db.mars_data
+    mars_data = Misssion_to_Mars.scrape()
     mars.update({}, mars_data, upset=True)
-    return "Scraping Successful"
+    return redirect("https://localhost:5000/", code=302)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
